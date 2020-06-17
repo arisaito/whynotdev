c6572450e013965cd931f6aa45d4b88fcda6051a
arisa ito <saharu54@live.jp>
2020-06-17 09:54:48 +0900

    make scene change1

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,23 @@ import "@fortawesome/fontawesome";
 import "@fortawesome/fontawesome-free-solid";
 
 let mobileFlag = false;
-let bg;
-let en;
+let colliderScene1 = [];
+let colliderScene2 = [];
+let colliderScene3 = [];
 let collider01;
+let collider02;
+let collider03;
+let collider04;
+let collider05;
+// let collider06;
+// let collider07;
+let ring1;
+let ringScene1to2;
+let ringScene1to3;
+let bg1;
+let bg2;
+let bg3;
+
 let windowWidth;
 let windowHeight;
 let loading;
@@ -30,25 +44,36 @@ let usageFlag = false;
 AFRAME.registerComponent("cursor-listener", {
   init: function () {
     cursorImg = document.getElementById("cursor-img");
-    this.el.addEventListener("click", function (evt) {
-      console.log("click");
-      location.href = "../works/towatakaya/001.html";
-    });
-    this.el.addEventListener("mouseenter", function (e) {
-      console.log("mouseenter");
-      cursorImg.setAttribute("scale", "0.1 0.1 0.1");
-      cursorImg.setAttribute(
-        "animation",
-        "property: opacity; from: 1.0; to: 0.0; dur: 500; loop: true; easing: linear; dir: alternate"
-      );
-    });
-    this.el.addEventListener("mouseleave", function (e) {
-      cursorImg.setAttribute("scale", "0.07 0.07 0.07");
-      cursorImg.removeAttribute("animation");
-      cursorImg.setAttribute("opacity", 1.0);
-    });
+    // this.el.addEventListener("click", function (evt) {
+    //   // location.href = "../works/towatakaya/001.html";
+    // });
+    this.el.addEventListener("mouseenter", mouseEnter);
+    this.el.addEventListener("mouseleave", mouseLeave);
+    this.el.addEventListener("click", clickEvent);
   },
 });
+
+const mouseEnter = () => {
+  console.log("mouseenter");
+  cursorImg.setAttribute("scale", "0.1 0.1 0.1");
+  cursorImg.setAttribute(
+    "animation",
+    "property: opacity; from: 1.0; to: 0.0; dur: 500; loop: true; easing: linear; dir: alternate"
+  );
+};
+
+const mouseLeave = () => {
+  cursorImg.setAttribute("scale", "0.07 0.07 0.07");
+  cursorImg.removeAttribute("animation");
+  cursorImg.setAttribute("opacity", 1.0);
+};
+
+const clickEvent = () => {
+  colliderScene1 = document.querySelectorAll(".collider-scene1");
+  console.log(this);
+  console.log("collider");
+  console.log("click");
+};
 
 const initDevice = () => {
   let regexp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
@@ -165,13 +190,39 @@ const hamburgerController = () => {
   });
 };
 
+const sceneController = () => {
+  ring1 = document.getElementById("ring1");
+  ringScene1to2 = document.getElementById("ring-scene-1to2");
+  ringScene1to3 = document.getElementById("ring-scene-1to3");
+  bg1 = document.getElementById("bg1");
+  bg2 = document.getElementById("bg2");
+  ringScene1to2.addEventListener("click", ringScene1to2Click);
+  ringScene1to3.addEventListener("click", ringScene1to3Click);
+};
+
+const ringScene1to2Click = () => {
+  console.log("scene2");
+  colliderScene1.forEach((el) => {
+    el.removeAttribute("cursor-listener");
+    el.removeEventListener("mouseenter", mouseEnter);
+    el.removeEventListener("click", clickEvent);
+    mouseLeave();
+  });
+  bg1.setAttribute("mixin", "fadeout");
+  bg2.setAttribute("mixin", "fadein");
+  ring1.setAttribute("mixin", "out");
+  ringScene1to2.removeEventListener("click", ringScene1to2Click);
+  ringScene1to3.removeEventListener("click", ringScene1to3Click);
+};
+
+const ringScene1to3Click = () => {
+  console.log("scene3");
+};
+
 window.onload = () => {
   initDevice();
   hideLoading();
   firstModalClose();
   hamburgerController();
-  //   collider01 = document.getElementById("collider01");
-  //   bg = document.getElementById("360bg");
-  //   en = document.getElementById("en01");
-  //   collider = document.getElementById("collider01");
+  sceneController();
 };
